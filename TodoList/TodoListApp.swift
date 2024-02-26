21ad0b6062977efc13ffff21b67e0815423276a8
Bhuvan Gundela <bhuvangundela@yahoo.com>
2024-02-26 11:29:35 -0600

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by 64020443 on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
